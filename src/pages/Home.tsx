111741dea4cc4921109b4010e8efc0a9e5c77205
Ayoe <dannyhopkins20003@gmail.com>
2023-05-15 23:23:24 +0100

    refactored homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,23 +2,22 @@ import Cards from "../components/Cards";
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 import Table from "../components/Table";
-import '../components/styles/home.scss'
+import "../components/styles/home.scss";
 
 const Home = () => {
   return (
-<main className="home">
-  <div className="home__navbar">
-    <Navbar />
-  </div>
-  <div className="home__details">
-      <Sidebar />
-    <div className="home__main">
-        <Cards />
-        <Table />
-    </div>
-  </div>
-</main>
-
+    <main className="home">
+      <div className="home__navbar">
+        <Navbar />
+      </div>
+      <div className="home__details">
+        <Sidebar />
+        <div className="home__main">
+          <Cards />
+          <Table />
+        </div>
+      </div>
+    </main>
   );
 };
 
